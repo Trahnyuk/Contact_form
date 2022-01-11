949a80149582a21cb4ac6c5aa492dd72693d2f5c
Petro Trahnyuk <peter.trahnyuk@gmail.com>
2022-01-11 16:32:00 +0100

    validation form with js... bagin

--- 3.0/validation.js
+++ 3.0/validation.js
@@ -1,5 +1,46 @@
-<script>
+
+        function regForm() {
+
+            const firstName =
+                document.getElementsByClassName["form"]["firstName"];
+
+            const lastName =
+                document.getElementsByClassName["form"]["lastName"];
+
+            const email =
+                document.getElementsByClassName["form"]["email"];
+
+            const phone =
+                document.getElementsByClassName["form"]["phone"];
+
+           
+
+            if (firstName.value == "") {
+                window.alert("Please enter your First name.");
+                firstName.focus();
+                return false;
+            }
+
+            if (lastName.value == "") {
+                window.alert("Please enter your Last name.");
+                lastName.focus();
+                return false;
+            }
 
 
-    
-</script>
+            if (email.value == "") {
+                window.alert(
+                "Please enter a valid e-mail address.");
+                email.focus();
+                return false;
+            }
+
+            if (phone.value == "") {
+                window.alert(
+                "Please enter your telephone number.");
+                phone.focus();
+                return false;
+            }
+
+            return true;
+        }
