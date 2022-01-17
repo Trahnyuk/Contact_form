4d991e12de1b43bff3b2c63c16dac26fddac4a1b
Petro Trahnyuk <peter.trahnyuk@gmail.com>
2022-01-17 12:59:54 +0100

    added phone validation in JS

--- 3.0/validation.js
+++ 3.0/validation.js
@@ -1,4 +1,4 @@
-         function check(){
+         function checkEmail(){
 
         const email = document.querySelector("#email");
         const icon1 = document.querySelector(".icon1");
@@ -7,10 +7,10 @@
         
         const btn = document.querySelector("contactForm__send_button");
 
-         let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+         let regularExprationEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 
-           if(email.value.match(regExp)){
+           if(email.value.match(regularExprationEmail)){
              email.style.borderColor = "#27ae60";
              email.style.background = "#eafaf1";
              icon1.style.display = "none";
@@ -39,16 +39,59 @@
          }
 
          
-         function check(){
-
-          const email = document.querySelector("#email");
-          const icon1 = document.querySelector(".icon1");
-          const icon2 = document.querySelector(".icon2");
+         function checkPhone()
+         {
+          const email = document.querySelector("#phone");
+          const icon1 = document.querySelector(".icon3");
+          const icon2 = document.querySelector(".icon4");
           const error = document.querySelector(".button");
           
           const btn = document.querySelector("contactForm__send_button");
   
-           let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+           let regularExprationPhone = /^\d{3}\d{3}\d{3}$/;
+  
+  
+             if(email.value.match(regularExprationPhone)){
+               email.style.borderColor = "#27ae60";
+               email.style.background = "#eafaf1";
+               icon1.style.display = "none";
+               icon2.style.display = "block";
+               error.style.display = "none";
+               btn.style.display = "block";
+             }
+             
+             else{
+               email.style.borderColor = "#e74c3c";
+               email.style.background = "#fceae9";
+               icon1.style.display = "block";
+               icon2.style.display = "none";
+               error.style.display = "block";
+               btn.style.display = "none";
+             }
+             
+             if(email.value == ""){
+               email.style.borderColor = "#eae2b7";
+               email.style.background = "#eae2b7f";
+               icon1.style.display = "none";
+               icon2.style.display = "none";
+               error.style.display = "none";
+               btn.style.display = "none";
+
+             }
+         }
+
+
+         /*
+         
+
+          const email = document.querySelector("#phone");
+          const icon1 = document.querySelector(".icon3");
+          const icon2 = document.querySelector(".icon4");
+          const error = document.querySelector(".button");
+          
+          const btn = document.querySelector("contactForm__send_button");
+  
+           let regExp = /^\d{3}-\d{3}-\d{3}-\d{3}$/;
   
   
              if(email.value.match(regExp)){
@@ -78,3 +121,4 @@
                btn.style.display = "none";
              }
            }
+           */
